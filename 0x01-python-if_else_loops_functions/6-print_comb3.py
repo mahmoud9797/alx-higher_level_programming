113f7b27d6face747e52af9e2cee61a11d07a905
Mahmoud <elmahallawy6@gmail.com>
2024-01-06 13:45:32 -0800

    mahmoud

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for n1 in range(0, 10):
         if n1 == 8 and n2 == 9:
             print("{}{}".format(n1, n2))
         else:
-            print("{}{}".format(n1, n2), end=" ,")
+            print("{}{}".format(n1, n2), end=", ")
