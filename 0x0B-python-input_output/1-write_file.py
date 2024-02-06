e5b966f673aee43a365e1a813f55d70a7bdb88c1
Mahmoud <elmahallawy6@gmail.com>
2024-02-06 15:57:01 -0800

    mahmoud

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" function used to write on text file"""
+
+
+def write_file(filename="", text=""):
+    """ two args : 
+    filename ( is the name of text file we will write on it
+    text is the text we would like to write it on the file"""
+
+    with open(filename, "w", encoding="utf-8") as txt_f:
+        return txt_f.write(text)
