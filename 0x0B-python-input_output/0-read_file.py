f0823fe4eb69ac0d1b0d3eab15d2425279b6cd00
Mahmoud <elmahallawy6@gmail.com>
2024-02-06 14:47:36 -0800

    mahmoud

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module to read text file"""
+
+
+def read_file(filename=""):
+    """ function take txt filename --->
+    open it ---> read it ---> print it --->"""
+    with open(filename, 'r', encoding="utf-8") as txt_f:
+        print(txt_f.read())
