3b3ba8379c28812864073306784271c36577d769
Mahmoud <elmahallawy6@gmail.com>
2024-02-28 19:00:36 -0800

    mahmoud

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+"""rectangle class module"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle calss inherint"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
