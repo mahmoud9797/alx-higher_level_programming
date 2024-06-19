e008063b8e16066ea8f721be34e95de07ae5918d
Mahmoud <elmahallawy6@gmail.com>
2024-06-19 04:38:27 +0300

    mahmoud

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+const argc = process.argv.length;
 
-argc = process.argv.length
-if (argc === 2){
-	console.log('No argument');
-}
-else if (argc === 3){
-	console.log('Argument found');
-}
-else{
-	console.log('Arguments found');
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
