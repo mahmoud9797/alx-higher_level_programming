59d02148e47aadfdf08fddebab37f0a19317e914
Mahmoud <elmahallawy6@gmail.com>
2024-06-20 04:28:41 +0300

    mahmoud

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.height = h;
+    if ((w > 0) && (h > 0)) {
       this.width = w;
+      this.height = h;
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let row = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
-        row += 'X';
+        s += 'X';
       }
-      console.log(row);
+      console.log(s);
     }
   }
 }
